5896c986e5a1d20c02ccd28ddf39c0d16114ab84
rfelixmg <rfelixmg@gmail.com>
2026-02-13 22:12:58 +0900

    small change, full of hope

--- frontend/src/lib/utils/api-utils.ts
+++ frontend/src/lib/utils/api-utils.ts
@@ -1,18 +1,19 @@
 export function getBaseUrl() {
   if (typeof window === 'undefined') {
     if (process.env.NODE_ENV === 'production') {
-      return process.env.NEXT_PUBLIC_API_BASE_URL || 'https://benkyfy.site';
+      // return process.env.NEXT_PUBLIC_API_BASE_URL || 'https://benkyfy.site';
+      return 'http://benkyfy.site';
     }
-    return process.env.NEXT_PUBLIC_API_BASE_URL || `http://localhost:3000`;
+    return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://benkyfy.site';
   }
   return '';
 }
 
 export function getBackendUrl() {
   if (process.env.NODE_ENV === 'production') {
-    return process.env.NEXT_PUBLIC_API_BASE_URL || 'https://benkyfy.site';
+    return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://benkyfy.site';
   }
-  return process.env.NEXT_PUBLIC_API_BASE_URL || `http://localhost:8080`;
+  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://benkyfy.site';
 }
 
 export async function fetchFromBackend(path: string, options: RequestInit = {}) {
