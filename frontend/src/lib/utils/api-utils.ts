3720dc9cf8e7caa34c596657a201f82d705f2ad8
rfelixmg <rfelixmg@gmail.com>
2026-02-14 07:47:47 +0900

    improving docker-compose-local

--- frontend/src/lib/utils/api-utils.ts
+++ frontend/src/lib/utils/api-utils.ts
@@ -1,19 +1,18 @@
 export function getBaseUrl() {
   if (typeof window === 'undefined') {
     if (process.env.NODE_ENV === 'production') {
-      // return process.env.NEXT_PUBLIC_API_BASE_URL || 'https://benkyfy.site';
-      return 'https://benkyfy.site';
+      return process.env.NEXT_PUBLIC_API_BASE_URL || 'https://benkyfy.site';
     }
-    return 'https://benkyfy.site'; //process.env.NEXT_PUBLIC_API_BASE_URL || 'http://benkyfy.site';
+    return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
   }
-  return 'https://benkyfy.site';
+  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
 }
 
 export function getBackendUrl() {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://benkyfy.site'; //process.env.NEXT_PUBLIC_API_BASE_URL || 'http://benkyfy.site';
+    return process.env.NEXT_PUBLIC_API_BASE_URL || 'https://benkyfy.site';
   }
-  return 'https://benkyfy.site'; //process.env.NEXT_PUBLIC_API_BASE_URL || 'http://benkyfy.site';
+  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
 }
 
 export async function fetchFromBackend(path: string, options: RequestInit = {}) {
