008a822ce93da37ecbca0d507f0a463b435d2a82
rfelixmg <rfelixmg@gmail.com>
2026-02-13 19:14:40 +0900

    quick change

--- frontend/src/lib/utils/api-utils.ts
+++ frontend/src/lib/utils/api-utils.ts
@@ -3,7 +3,7 @@ export function getBaseUrl() {
     if (process.env.NODE_ENV === 'production') {
       return process.env.NEXT_PUBLIC_API_BASE_URL || '';
     }
-    return process.env.NEXT_PUBLIC_API_BASE_URL || `http://benkyfy-frontend:${process.env.PORT || 3000}`;
+    return process.env.NEXT_PUBLIC_API_BASE_URL || `http://frontend:${process.env.PORT || 3000}`;
   }
   return '';
 }
@@ -12,7 +12,7 @@ export function getBackendUrl() {
   if (process.env.NODE_ENV === 'production') {
     return process.env.NEXT_PUBLIC_API_BASE_URL || '';
   }
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://benkyfy-backend:8080';
+  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://backend:8080';
 }
 
 export async function fetchFromBackend(path: string, options: RequestInit = {}) {
