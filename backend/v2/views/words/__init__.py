1f1655bf2dbc6bb421c404ea40fb98adf4c4960f
rfelixmg <rfelixmg@gmail.com>
2026-02-14 17:20:49 +0900

    yes

--- backend/v2/views/words/__init__.py
+++ backend/v2/views/words/__init__.py
@@ -1,7 +1,8 @@
 from flask_restx import Namespace
 
+ns = Namespace("words", description="Words operations")
+
 from . import crud, study, wlist
 
-ns = Namespace("words", description="Words operations")
 
 __all__ = ["ns", "crud", "study", "wlist"]
